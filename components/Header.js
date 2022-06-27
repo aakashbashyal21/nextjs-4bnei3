ded3f7116e08609c9c10c5cc227940b04307b6a5
aakashbashyal21 <aakashbashyal@gmail.com>
2022-06-27 17:32:36 +0545

    4 files modified, 1 deleted

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,6 @@
 export default function Header() {
   return (
     <header className="header-nav">
-      <p>Hi From header</p>
       <div className="container">
         <div className="row">
           <div className="col-12">
@@ -15,11 +14,173 @@ export default function Header() {
                   className="img-fluid"
                   width="110"
                   height="35"
-                  src="assets/images/logo.png"
+                  src="https://upload.wikimedia.org/wikipedia/commons/5/52/Logo_de_Free.png"
                   alt="Qurno"
                 />
-                logo
               </a>
+              <button
+                className="search-toggle d-inline-block d-lg-none ms-auto me-1 me-sm-3"
+                data-toggle="search"
+                aria-label="Search Toggle"
+              >
+                <span>Search</span>
+                <svg
+                  width="22"
+                  height="22"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="../../www.w3.org/2000/svg.html"
+                >
+                  <path
+                    d="M15.5 15.5L19 19"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navHeader"
+                aria-controls="navHeader"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i class="ti ti-menu-2 menu-open"></i>
+                <i class="ti ti-x menu-close"></i>
+              </button>
+              <div className="collapse navbar-collapse" id="navHeader">
+                <ul className="navbar-nav mx-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="index-2.html">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item @@about">
+                    <a className="nav-link" href="about.html">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item @@elements">
+                    <a className="nav-link" href="elements.html">
+                      Elements
+                    </a>
+                  </li>
+                  <li className="nav-item @@archive">
+                    <a className="nav-link" href="archive.html">
+                      Archive
+                    </a>
+                  </li>
+                  <li className="nav-item @@contact">
+                    <a className="nav-link" href="contact.html">
+                      Contact
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Pages
+                    </a>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                      <li>
+                        <a className="dropdown-item" href="author.html">
+                          Author
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="author-single.html">
+                          Author Single
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="tags.html">
+                          Tags
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="tag-single.html">
+                          Tag Single
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="categories.html">
+                          Categories
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          className="dropdown-item"
+                          href="categories-single.html"
+                        >
+                          Category Single
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="404-page.html">
+                          404 Page
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="privacy.html">
+                          Privacy
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                <div className="navbar-right d-none d-lg-inline-block">
+                  <ul className="social-links list-unstyled list-inline">
+                    <li className="list-inline-item ms-4 d-none d-lg-inline-block">
+                      <button
+                        className="search-toggle"
+                        data-toggle="search"
+                        aria-label="Search Toggle"
+                      >
+                        <span>Search</span>
+                        <svg
+                          width="22"
+                          height="22"
+                          stroke-width="1.5"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="../../www.w3.org/2000/svg.html"
+                        >
+                          <path
+                            d="M15.5 15.5L19 19"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                          <path
+                            d="M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          />
+                        </svg>
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </nav>
           </div>
         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 import '../styles/globals.css';
+
+import { useEffect } from 'react';
+
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
   return (
     <>
       <Header />
