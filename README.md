6731c633dd85490b807d0f27bb965840d976d760
aakashbashyal21 <aakashbashyal@gmail.com>
2022-06-27 16:00:32 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-4bnei3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-4bnei3)
---
+++ components/Header.js
@@ -0,0 +1,29 @@
+export default function Header() {
+  return (
+    <header className="header-nav">
+      <p>Hi From header</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="navbar navbar-expand-lg navbar-light p-0">
+              <a
+                className="navbar-brand font-weight-bold mb-0"
+                href="index-2.html"
+                title="Qurno"
+              >
+                <img
+                  className="img-fluid"
+                  width="110"
+                  height="35"
+                  src="assets/images/logo.png"
+                  alt="Qurno"
+                />
+                logo
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
